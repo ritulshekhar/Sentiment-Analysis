8cb8fa3877db8315b2221efd69089f5dd19c6863
Ritul Shekhar <ritulshekhar@gmail.com>
2025-07-08 20:26:15 +0530

    Tokenising

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from utils.preprocessing import clean_text
+import pickle
+
+model = load_model("model/sentiment_model.h5")
+
+with open("model/tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+def predict_sentiment(text):
+    cleaned = clean_text(text)
+    seq = tokenizer.texts_to_sequences([cleaned])
+    padded = pad_sequences(seq, maxlen=100)
+    pred = model.predict(padded)
+    return ['Negative', 'Neutral', 'Positive'][pred.argmax()]
+
+if __name__ == "__main__":
+    text = input("Enter tweet: ")
+    print("Sentiment:", predict_sentiment(text))
